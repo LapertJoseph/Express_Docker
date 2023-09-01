5e678ba3e15e871d1fe7f8dbdc4880f0c9059966
lapertJoseph <lapertjoseph@gmail.com>
2023-09-01 11:51:48 +0200

    deleteUser  procedure

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,11 +6,16 @@
 const express = require('express');
 const get = require('../controllers/user/get');
 const post = require('../controllers/user/post');
+const del = require('../controllers/user/del');
+
 
 const router = express.Router();
 
 router.get("/users", get);
 
 router.post("/user", post);
+
+router.delete("/user", del);
+
 
 module.exports = router;
