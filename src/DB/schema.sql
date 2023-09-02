fbfa9762d5cbc2d0352cabdb4f095aea521e4f70
LapertJoseph <lapertjoseph@gmail.com>
2023-09-02 09:26:18 +0200

    squash

--- src/DB/schema.sql
+++ src/DB/schema.sql
@@ -1,3 +1,6 @@
+CREATE OR REPLACE DATABASE `expressdb`;
+use `expressdb`;
+
 CREATE OR REPLACE TABLE user (
   `user_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `name` VARCHAR(255),
@@ -10,12 +13,15 @@ CREATE OR REPLACE TABLE user (
 CREATE OR REPLACE TABLE card (
   `card_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `card_name` VARCHAR(255),
+  `card_monster_type` VARCHAR(255),
   `card_stars` INT(11),
+  `card_attribute` VARCHAR(255),
   `card_attack` INT(11),
   `card_defense` INT(11),
   `card_effect` TINYINT(1),
   `card_skill` VARCHAR(255),
   `card_image_url` VARCHAR(255),
+  `card_text` VARCHAR(255),
   `user_id` INT(11),
   CONSTRAINT `fk_card_user`
   FOREIGN KEY (`user_id`) REFERENCES user (`user_id`)
