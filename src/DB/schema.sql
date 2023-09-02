66065b355916e61519e845005ae9cdbf1573b510
LapertJoseph <lapertjoseph@gmail.com>
2023-09-02 09:00:54 +0200

    modif readme + new table sql card

--- src/DB/schema.sql
+++ src/DB/schema.sql
@@ -6,3 +6,18 @@ CREATE OR REPLACE TABLE user (
   `password` VARCHAR(255),
   `actif` TINYINT(1)
 );
+
+CREATE OR REPLACE TABLE card (
+  `card_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `card_name` VARCHAR(255),
+  `card_stars` INT(11),
+  `card_attack` INT(11),
+  `card_defense` INT(11),
+  `card_effect` TINYINT(1),
+  `card_skill` VARCHAR(255),
+  `card_image_url` VARCHAR(255),
+  `user_id` INT(11),
+  CONSTRAINT `fk_card_user`
+  FOREIGN KEY (`user_id`) REFERENCES user (`user_id`)
+  ON DELETE NO ACTION ON UPDATE NO ACTION
+);
